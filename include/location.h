e445bc14561e026e4080a3931c6f574c027c2b0e
e-y-e <e_y_e@tuta.io>
2018-03-04 09:46:36 +0000

    Add untracked files ready to replace point.h with location.h

---
+++ include/location.h
@@ -0,0 +1,40 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+
+#include <stdbool.h>
+#include <stddef.h>
+
+
+/**
+ * Represents a location of a node in a rectangular maze.
+ *
+ * This struct is a simple representation of the row and column that define the
+ * location of a node. The row and column are both unsigned, as they cannot be
+ * negative.
+ */
+struct location_t
+{
+    size_t row;
+    size_t column;
+};
+
+/**
+ * Calculates a consistent measure of the distance between two locations.
+ *
+ * This function simply calculates the square of the straight-line distance
+ * between the two locations, since taking the square root of integers would
+ * impose an unnecessary overhead. This measure is still a consistent heuristic,
+ * because the ordering is retained.
+ */
+size_t location_distance(struct location_t, struct location_t);
+
+/**
+ * Determines if two locations are equal to each other.
+ *
+ * Simply compares the row and column of each location for exact equality.
+ */
+bool location_equal(struct location_t, struct location_t);
+
+
+#endif // LOCATION_H
---
+++ location.c
@@ -0,0 +1,32 @@
+#include "location.h"
+
+
+/**
+ * Calculates the absolute difference between two unsigned integers.
+ *
+ * This helper function allows the effective distance between two unsigned
+ * integers to be calculated without having to convert to a signed
+ * representation.
+ */
+static size_t abs_difference(size_t a, size_t b)
+{
+    return a < b ? b - a : a - b;
+}
+
+// Define location_distance (location.h).
+size_t location_distance(struct location_t a, struct location_t b)
+{
+    // Calculate the distances on each axis.
+    size_t dx = abs_difference(a.row, b.row);
+    size_t dy = abs_difference(a.column, b.column);
+
+    // Combine the axis distances to obtain the square of the straight-line
+    // distance.
+    return dx * dx + dy * dy;
+}
+
+// Define location_equal (location.h).
+bool location_equal(struct location_t a, struct location_t b)
+{
+    return a.x == b.x && a.y == b.y;
+}
